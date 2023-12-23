cc0e5d615d50e87216061c3d5dbf51ed3386fcb2
NicholasCWK <chuanicholas2002@gmail.com>
2023-12-23 16:59:53 +0800

    add UI

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => playRound("rock"));
+paper.addEventListener("click", () => playRound("paper"));
+scissors.addEventListener("click", () => playRound("scissors"));
+
+const scoreDisplay = document.querySelector("#score");
+const result = document.querySelector("#result");
+const body = document.querySelector("body");
+
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+function playRound(playerSelection) {
+    computerSelection = getComputerChoice();
+    const div = document.createElement("div");
     if (playerSelection == computerSelection) {
-        return "Tie!";
+        div.textContent = "Tie!";
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return "You Win! Paper beats Rock";
+            div.textContent = "You Win! Paper beats Rock";
+            updateScore("player");
         } else if (computerSelection == "scissors") {
-            return "You Lose! Scissors beats Paper";
+            div.textContent = "You Lose! Scissors beats Paper";
+            updateScore("computer");
         }
     } else if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
-            return "You Win! Rock beats Scissors";
+            div.textContent = "You Win! Rock beats Scissors";
+            updateScore("player");
         } else if (computerSelection == "paper") {
-            return "You Lose! Paper beats Rock";
-        }
-    } else if (playerSelection == "scissors") {
-        if (computerSelection == "paper") {
-            return "You Win! Scissors beats Paper";
-        } else if (computerSelection == "rock") {
-            return "You Lose! Rock beats Scissors";
+            div.textContent = "You Lose! Paper beats Rock";
+            updateScore("computer");
         }
     } else {
-        return "Invalid input";
+        if (computerSelection == "paper") {
+            div.textContent = "You Win! Scissors beats Paper";
+            updateScore("player");
+        } else if (computerSelection == "rock") {
+            div.textContent = "You Lose! Rock beats Scissors";
+            updateScore("computer");
+        }
+    }
+    const result = document.querySelector("#result");
+    result.appendChild(div);
+}
+
+function updateScore(winner) {
+    if (winner == "player") {
+        playerScore++;
+    } else {
+        computerScore++;
+    }
+    scoreDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    if (playerScore >= 5) {
+        endGame(true);
+    } else if (computerScore >= 5) {
+        endGame(false);
     }
 }
 
-function game() {
-    const playerSelection = prompt("Choose between Rock, Paper, or Scissors");
-    const computerSelection = getComputerChoice();
-    console.log("Player choice: " + playerSelection);
-    console.log("Computer choice " + computerSelection);
-    return playRound(playerSelection, computerSelection);
-}
-
-for (let i = 1; i < 6; i++) {
-    console.log("Round " + i + ": " + game());
+function endGame(playerWins) {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+    const div = document.createElement("div");
+    if (playerWins) {
+        div.textContent = "You win the game!";
+    } else {
+        div.textContent = "You lose the game!";
+    }
+    body.appendChild(div);
 }
